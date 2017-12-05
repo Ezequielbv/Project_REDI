4028f1ca92bf9d3d627f6f8cb173605b27871af9
Ezequielbv <ezequielbv@gmail.com>
2017-12-05 21:18:51 +0100

    Fix bugs

--- js/addclickhandler.js
+++ js/addclickhandler.js
@@ -1,11 +1,13 @@
 window.REDI = window.REDI || {};
 
 REDI.addClickHandler = function (IDs, dbRecipes) {
+	let oldClickHandler = null;
+
   document.getElementById('submit').addEventListener('click', function(){
     let recipes = REDI.getRecipes(IDs, dbRecipes),
     	index = REDI.displayRecipes(recipes);
 
-    REDI.noLike(recipes, index);
+    oldClickHandler = REDI.noLike(recipes, index, oldClickHandler);
   });
 
 };
--- js/nolike.js
+++ js/nolike.js
@@ -1,13 +1,21 @@
 window.REDI = window.REDI || {};
 
-REDI.noLike = function (recipes, index) {
-	let btn = document.getElementById("noLike");
+REDI.noLike = function (recipes, index, oldClickHandler) {
+	let btn = document.getElementById("noLike"),
+		clickHandler = function () {
+			recipes.splice(index, 1);
+			index = REDI.displayRecipes(recipes);
+			if (recipes.length === 1) {
+				btn.style.display = 'none';
+			}
+		};
+
 	btn.style.display = 'inline';
-	btn.addEventListener('click', function(){
-		recipes.splice(index, 1);
-		index = REDI.displayRecipes(recipes);
-		if (recipes.length === 1) {
-			btn.style.display = 'none';
-		}
-	});
+
+	if (typeof oldClickHandler === "function") {
+		btn.removeEventListener('click', oldClickHandler);
+	}
+
+	btn.addEventListener('click', clickHandler);
+	return clickHandler;
 };
